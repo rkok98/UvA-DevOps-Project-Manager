d7fe3081e29910cb8851e5d9d608b86bee650b79
René Kok <rkok98@gmail.com>
2023-02-13 15:18:59 +0100

    Initial commit

---
+++ test/devops-project-manager.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevopsProjectManager from '../lib/devops-project-manager-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/devops-project-manager-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevopsProjectManager.DevopsProjectManagerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
