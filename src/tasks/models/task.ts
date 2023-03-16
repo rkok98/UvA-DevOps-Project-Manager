cfa1675241fa03a1d8996cf471636c5a4546c061
Tim van Ekert <tim.van.ekert@hva.nl>
2023-03-16 23:54:03 +0100

    feat: creating tasks succeeded

---
+++ src/tasks/models/task.ts
@@ -0,0 +1,9 @@
+// The task object model, i.e., format of a new task.
+export interface Task {
+  id: string;
+  projectId: string;
+  title: string;
+  description: string;
+  dateTime: string;
+  createdBy: string;
+}
