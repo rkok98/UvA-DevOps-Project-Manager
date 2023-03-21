bd4c333ce5e2adc35442ed56248bc9536cbb04af
René Kok <rkok98@gmail.com>
2023-03-21 15:07:46 +0100

    test: add create task handler integration test

---
+++ test/tasks/handlers/create-task.test.ts
@@ -0,0 +1,171 @@
+import handler from '../../../src/tasks/handlers/create-task-handler';
+import { APIGatewayProxyResult, Callback } from 'aws-lambda';
+import { HttpResponse } from '../../../src/util/http-response';
+import {
+  mockContext,
+  mockEvent,
+  mockRequestContext,
+} from '../../util/handler-utils';
+import { mockClient } from 'aws-sdk-client-mock';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
+
+const backupEnv = process.env;
+const ddbMock = mockClient(DynamoDBClient);
+
+describe('Create Task Handler Integration Tests', () => {
+  beforeEach(() => {
+    jest.resetModules();
+
+    ddbMock.reset();
+    process.env = {
+      ...backupEnv,
+      AWS_REGION: 'eu-west-1',
+      DYNAMODB_TABLE_NAME: 'project-table',
+    };
+  });
+
+  afterEach(() => {
+    process.env = backupEnv;
+  });
+
+  test('Missing AWS_REGION should return internal server error', async () => {
+    process.env.AWS_REGION = undefined;
+
+    const res = await handler(
+      mockEvent,
+      mockContext,
+      {} as Callback<APIGatewayProxyResult>
+    );
+
+    expect(res).toEqual(
+      HttpResponse.internalServerError(
+        'AWS_REGION was not specified in the environment variables'
+      )
+    );
+  });
+
+  test('Missing DYNAMODB_TABLE_NAME should return internal server error', async () => {
+    process.env.DYNAMODB_TABLE_NAME = undefined;
+
+    const res = await handler(
+      mockEvent,
+      mockContext,
+      {} as Callback<APIGatewayProxyResult>
+    );
+
+    expect(res).toEqual(
+      HttpResponse.internalServerError(
+        'DYNAMODB_TABLE_NAME was not specified in the environment variables'
+      )
+    );
+  });
+
+  test('Missing sub should return internal server error', async () => {
+    const event = {
+      ...mockEvent,
+      requestContext: {
+        ...mockRequestContext,
+        authorizer: {
+          claims: {
+            sub: undefined,
+          },
+        },
+      },
+    };
+
+    const res = await handler(
+      event,
+      mockContext,
+      {} as Callback<APIGatewayProxyResult>
+    );
+
+    expect(res).toEqual(
+      HttpResponse.internalServerError('Something went wrong')
+    );
+  });
+
+  test('Missing Project ID in URL Path Parameters returns Bad Request', async () => {
+    const event = {
+      ...mockEvent,
+      requestContext: {
+        ...mockRequestContext,
+        authorizer: {
+          claims: {
+            sub: 'test-admin-id',
+          },
+        },
+      },
+      pathParameters: {
+        project_id: undefined,
+      },
+    };
+
+    const res = await handler(
+      event,
+      mockContext,
+      {} as Callback<APIGatewayProxyResult>
+    );
+
+    expect(res).toEqual(HttpResponse.badRequest('Project ID cannot be empty'));
+  });
+
+  test('Empty body returns bad request', async () => {
+    const event = {
+      ...mockEvent,
+      requestContext: {
+        ...mockRequestContext,
+        authorizer: {
+          claims: {
+            sub: 'test-admin-id',
+          },
+        },
+      },
+      pathParameters: {
+        project_id: 'test-project-id',
+      },
+      body: null,
+    };
+
+    const res = await handler(
+      event,
+      mockContext,
+      {} as Callback<APIGatewayProxyResult>
+    );
+
+    expect(res).toEqual(
+      HttpResponse.badRequest('Request body cannot be empty')
+    );
+  });
+
+  test('Successful returns created', async () => {
+    const event = {
+      ...mockEvent,
+      requestContext: {
+        ...mockRequestContext,
+        authorizer: {
+          claims: {
+            sub: 'test-admin-id',
+          },
+        },
+      },
+      pathParameters: {
+        project_id: 'test-project-id',
+      },
+      body: JSON.stringify({
+        title: 'Test task',
+        description: 'Test description',
+        state: 'To-Do',
+      }),
+    };
+
+    ddbMock.on(PutItemCommand).resolves({});
+
+    const res = await handler(
+      event,
+      mockContext,
+      {} as Callback<APIGatewayProxyResult>
+    );
+
+    expect(res).toEqual(HttpResponse.created());
+  });
+});
